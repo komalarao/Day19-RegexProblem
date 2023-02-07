37398aa9258edfac6d73f96785cf88b11669a937
komalarao <manoharvakamullu1999@gmail.com>
2023-02-07 02:06:26 -0800

    added: regex pattern to check user lasttName

--- src/com/bridgelabz/regexProblems/UserRegistration.java
+++ src/com/bridgelabz/regexProblems/UserRegistration.java
@@ -1,25 +1,27 @@
 package com.bridgelabz.regexProblems;
+
 import java.util.regex.*;
 import java.util.*;
+
 public class UserRegistration {
 
 	static boolean userName(String regex, String input) {
-				return Pattern.compile(regex).matcher(input).matches();		
+		return Pattern.compile(regex).matcher(input).matches();
 	}
 
 	public static void main(String[] args) {
 		System.out.println("welcome to User Registration program");
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("enter your first name:");
-        String	input = scanner.next();
-        String	regex = "^[A-Z]{1}+[a-z]{2,}+$";
-		userName(regex,input);
-		
-		if(input.matches(regex) == true)
-			System.out.println("valid first name");
-		else	
-			System.out.println("invalid first name");
 
-}
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter your last name:");
+		String input = scanner.next();
+		String regex = "^[A-Z]{1}+[a-z]{2,}+$";
+		userName(regex, input);
+
+		if (input.matches(regex) == true)
+			System.out.println("valid last name");
+		else
+			System.out.println("invalid last name");
+
+	}
 }
